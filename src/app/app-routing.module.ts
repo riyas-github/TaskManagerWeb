edf4f76a2d1aff40ca0187f26f11e37f2da667e6
riyaaz <rian.mailme@gmail.com>
2019-05-16 17:48:43 +0800

    Initial Commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AddTaskComponent }  from './taskmanager/addtask/addtask.component';
+import { ListTaskComponent } from './taskmanager/listtask/listtask.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {
+    path : 'addtask',
+    component : AddTaskComponent
+  },
+  {
+    path: 'listtask',
+    component : ListTaskComponent
+  },
+  {
+    path : '',
+    redirectTo : '/addtask',
+    pathMatch : 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
+import { FormsModule, ReactiveFormsModule  } from '@angular/forms';
+import { AddTaskComponent } from './taskmanager/addtask/addtask.component';
+import { ListTaskComponent } from './taskmanager/listtask/listtask.component';
+import { Ng5SliderModule } from 'ng5-slider';
 import { AppComponent } from './app.component';
+import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
+// import { MatSliderModule } from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+// import {MatSliderModule} from '@angular/material/slider';
+import {TaskManagerSerivce } from './taskmanager/taskmanager.service';
+import { AppRoutingModule } from './app-routing.module';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddTaskComponent,
+    ListTaskComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    Ng5SliderModule,
+    ReactiveFormsModule,
+    FormsModule,
+     BsDatepickerModule.forRoot(),
+     BrowserAnimationsModule
   ],
-  providers: [],
+  providers: [TaskManagerSerivce],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
